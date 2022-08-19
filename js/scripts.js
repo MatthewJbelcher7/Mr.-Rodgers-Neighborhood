a30a803f367ef11e6ce34004ba2de9d3af2240fd
Matthew Belcher <matthewjbelcher7@live.com>
2022-08-19 14:31:00 -0600

    add else if, else

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
 // UI Logic
 pickNumber();
+
 function pickNumber(){
 
-  friendsNumber = prompt("Pick a number 0-10000 friend: ");
+  friendsNumber = prompt("Pick a number 1-50 friend: ");
 
-  if(friendsNumber) >= 0 && friendsNumber <=10000){
+  if(friendsNumber >= 1 && friendsNumber <=50){
 
   listNum();
-    
 
+  }else if (friendsnumber < 1){
+    alert("That number is to low friend... lets try to be more managable");
+    friendsNumber = 0;
+    pickNumber();
+  
+  }else if(friendsNumber > 50){
+    alert("We get so wrapped up in numbers in our society, unfortunately this number is to high ");
+    friendsNumber = 0; 
+    pickNumber();  
+  
+  }else{
+		alert("That is not a number!"); 
+		friendsNumber = 0;
+		pickNumber();
+	}
+}
 
 function placeNumbers(){
   do
